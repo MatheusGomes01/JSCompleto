5be4e95f4e904e974832259b377a9995cd38336a
MatheusGomes01 <matheus01gomes01ferreira2001@gmail.com>
2018-12-02 14:54:35 -0200

    contador de caracteres

--- contando_caracteres.php
+++ contando_caracteres.php
@@ -7,25 +7,37 @@
 
 	<div class="container">
 		<h1>JavaScript</h1>
-		<textarea name="msg" id="msg" cols="20" rows="10"></textarea>
-		<br>
-		Restam <span id="resto"></span> Caractesres
+		<textarea name="msg" id="msg" cols="20" rows="10" maxlength="200"></textarea>
+		<div id="restoContainer" style="display: none;">
+			Restam <span id="resto"></span> Caractesres
+		</div>
 	</div>
 
 	<script type="text/javascript">
 
 		(()=>{
 			'use strict';
-			var maximo = 100;
-			var	txtMsg = document.querySelector('#msg');
-			var	resto = document.querySelector('#resto');
+			
+			let	txtMsg = document.querySelector('#msg');
+			let restoContainer = document.querySelector('#restoContainer');
+			let	resto = document.querySelector('#resto');
+			let maximo = txtMsg.maxLength;
 
-			resto.innerHTML = maximo;
+			restoContainer.style.display = "block";
+
+			showCaracteres(maximo)
 			txtMsg.addEventListener('input', checkLength);
 
 			function checkLength(e){
+
 				var numero = this.value.length;
-				resto.innerHTML = maximo - numero;
+				var CaracteresRestantes = parseInt(maximo) - parseInt(numero);
+
+				showCaracteres(CaracteresRestantes);
+			}
+
+			function showCaracteres(n){
+				resto.innerHTML = n;
 			}
 		})()
 	</script>
